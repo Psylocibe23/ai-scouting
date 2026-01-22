1df9b2102447589ff9c9e9bf06ffbc9033c13f98
Psylocibe23 <spreafichi.f@gmail.com>
2026-01-22 19:36:04 +0100

    Add custome menu with template functions to UI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 function onOpen() {
   const ui = SpreadsheetApp.getUi();
   ui.createMenu('Startup Scouting AI')
-    .addItem('Hello test', 'helloTest')
+    .addItem('Scouting Accelerators', 'runScoutingAccelerators')
+    .addItem('Update Startups', 'runUpdateStartups')
+    .addItem('Generate Value Propositions', 'runGenerateValueProps')
+    .addItem('Export CSVs', 'runExportCsv')
     .addToUi();
 }
 
-function helloTest() {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  ss.toast('Hello from TypeScript + clasp setup!');
+function runScoutingAccelerators() {
+    const SpreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+    SpreadSheet.toast("runScoutingAccelerators called correctly.")
+}
+
+function runUpdateStartups() {
+    const SpreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+    SpreadSheet.toast("runUpdateStartups called correctly.")
+}
+
+function runGenerateValueProps() {
+    const SpreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+    SpreadSheet.toast("runGenerateValueProps called correctly.")
+}
+
+function runExportCsv() {
+    const SpreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+    SpreadSheet.toast("runExportCsv called correctly.")
 }
